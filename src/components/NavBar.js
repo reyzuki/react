b84544457b9ffd8a0ff8db7aeeebe52584cd971f
reyzuki <dboakye379@gmail.com>
2022-08-12 08:35:40 +0000

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>
+      <h1>Welcome to my shop</h1>
+      <nav>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Products</li>
+            <li>Contact</li>
+
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
